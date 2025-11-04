83fdec198e599ce0eb328692b3d324531d03e978
xzy-bit <buxzyhangaa@gamil.com>
2025-11-04 12:12:07 +0800

    add metrics

--- src/dpo.sh
+++ src/dpo.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 export CUDA_LAUNCH_BLOCKING=1
-MODEL="pythia410m"
-#MODEL="qwen18"
+#MODEL="pythia410m"
+MODEL="qwen18"
 #DATASET="ultrafb"
 DATASET="hh"
 N_EPOCHS=6
@@ -10,10 +10,10 @@ python -u train.py \
     loss=dpo \
     loss.beta=0.1 \
     model=$MODEL \
-    exp_name="base_dpo_${DATASET}_${MODEL}_ep${N_EPOCHS}_testargmax_sft"\
+    exp_name="base_dpo_${DATASET}_${MODEL}_ep${N_EPOCHS}_new_metrics"\
     trainer=BasicTrainer \
     n_epochs=$N_EPOCHS \
     n_examples=30000 \
-    model.archive="base_${DATASET}_sft_${MODEL}_ep8" \
+    model.archive="base_sft_${MODEL}_ep8" \
     save_ckp=true \
     eval_every=1000
