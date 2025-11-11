5a22b57ffc1d019b8f779916e6f1039735fa5fc0
xzy-bit <buxzyhangaa@gamil.com>
2025-11-11 11:25:37 +0800

    gen task

--- src/gen_dpo.sh
+++ src/gen_dpo.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1
 python -u gen_multipt.py \
 	model=qwen18\
-	model.archive=sparse_dpo_qwen18_ep6\
-	exp_name=eval_sparse_dpo_qwen18
+	model.archive=masked_dpo_hh_qwen18_ep6\
+	exp_name=eval_masked_dpo_hh_qwen_ep6_11_11
