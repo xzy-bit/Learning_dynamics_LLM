d2231cff023c110a328fb036674989c641e371c6
xzy-bit <buxzyhangaa@gamil.com>
2025-12-01 19:23:59 +0800

    Results-121

--- src/gen_dpo.sh
+++ src/gen_dpo.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1
 python -u gen_multipt.py \
 	model=qwen18\
-	model.archive=dpo_masked_qwen18_hh_ep6\
-	exp_name=eval_dpo_masked_hh_qwen18
+	model.archive=dpo_base_qwen18_hh_ep6\
+	exp_name=eval_dpo_base_hh_qwen18
