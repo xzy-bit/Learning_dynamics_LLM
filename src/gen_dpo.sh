6b484f10a77e18e9c2e10bab649c1cfdddc68e4f
xzy-bit <buxzyhangaa@gamil.com>
2025-12-03 15:51:32 +0800

    reproduce exp

--- src/gen_dpo.sh
+++ src/gen_dpo.sh
@@ -1,5 +1,15 @@
 export CUDA_VISIBLE_DEVICES=2
 python -u gen_multipt.py \
 	model=qwen18\
-	model.archive=dpo_base_qwen18_hh_ep6\
-	exp_name=eval_dpo_base_hh_qwen18
+	model.archive=dpo_base_qwen18_hh_ep6_1202\
+	exp_name=eval_dpo_base_qwen18_hh_1202
+
+python -u gen_multipt.py \
+        model=qwen18\
+        model.archive=dpo_masked_qwen18_hh_ep6_1202\
+        exp_name=eval_dpo_masked_qwen18_hh_1202
+
+python -u gen_multipt.py \
+        model=qwen18\
+        model.archive=dpo_extend_qwen18_hh_ep6_1202\
+        exp_name=eval_dpo_extend_qwen18_hh_1202
--- src/gen_sft.sh
+++ src/gen_sft.sh
@@ -1,5 +1,10 @@
 export CUDA_VISIBLE_DEVICES=3
 python -u gen_inference_samples.py \
 	model=qwen18\
-	model.archive=extend_sft_qwen18_ep4\
-	exp_name=eval_extend_sft_qwen18
+	model.archive=sft_qwen18_hh_ep2_1202\
+	exp_name=eval_sft_qwen18_hh_ep2_1202
+
+python -u gen_inference_samples.py \
+        model=qwen18\
+        model.archive=extend_sft_qwen18_hh_ep1_1202 \
+        exp_name=eval_extend_sft_qwen18_hh_ep1_1202
