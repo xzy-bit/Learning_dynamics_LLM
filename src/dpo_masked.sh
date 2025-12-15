1c79d448642d68d81461da5f2b146708eb824f41
xzy-bit <buxzyhangaa@gamil.com>
2025-12-15 11:54:01 +0800

    debug exps

--- src/dpo_masked.sh
+++ src/dpo_masked.sh
@@ -14,7 +14,7 @@ python -u train.py \
     loss.beta=0.1 \
     datasets=$DATASET \
     model=$MODEL \
-    exp_name="dpo_gated_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
+    exp_name="dpo_debug_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
     trainer=BasicTrainer \
     n_epochs=$N_EPOCHS \
     n_examples=$N_EXAMPLES \
--- src/gen_dpo_masked.sh
+++ src/gen_dpo_masked.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
 python -u gen_multipt.py \
 	model=qwen18\
-	model.archive=dpo_gated_qwen18_hh_ep16_1214\
-	exp_name=eval_gated_qwen18_hh_ep16_1214
+	model.archive=dpo_debug_qwen18_hh_ep16_1214\
+	exp_name=eval_debug_qwen18_hh_ep16_1214
