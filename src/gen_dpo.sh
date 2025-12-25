59200e3449702c36f3f84030c5b138ac9cac9d93
xzy-bit <buxzyhangaa@gamil.com>
2025-12-25 19:30:28 +0800

    1225-3

--- src/gen_dpo.sh
+++ src/gen_dpo.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
 python -u gen_multipt.py \
 	model=qwen18\
-	model.archive=dpo_asym_qwen18_hh_ep6_1225\
-	exp_name=eval_dpo_asym_qwen18_hh_ep6_1225
+	model.archive=dpo_asym_qwen18_hh_ep6_1225_1000000\
+	exp_name=eval_dpo_asym_qwen18_hh_ep6_1225_1000000
