9ac094c3a85fa989fbc6554b263ef415e0cc6ab6
xzy-bit <buxzyhangaa@gamil.com>
2025-12-30 16:10:00 +0800

    1230

--- src/auto.sh
+++ src/auto.sh
@@ -10,21 +10,21 @@ SFT_EPOCHS=2
 N_EPOCHS=6
 N_EXAMPLES=30000
 EVAL_EVERY=100000
-DATE="1226"
+DATE="1229"
 
 MASK_TYPE="hard_threshold"
 MASK_TOP_K=0
 MASK_THRESHOLD_PROB=0.1
 MASK_RATIO=0.1
 
-MASK_STRENGTHS=(20.0 30.0 40.0 50.0)
+MASK_STRENGTHS=(40.0 50.0)
 
 for MASK_STRENGTH in "${MASK_STRENGTHS[@]}"; do
     echo "======================================================"
-    echo "Running Mask-DPO with mask_ratio=${MASK_STRENGTH}"
+    echo "Running Mask-DPO"
     echo "======================================================"
 
-    EXP_NAME="dpo_mask_log1mp_${MASK_TYPE}_r_${MASK_RATIO}_k_${MASK_TOP_K}_s_${MASK_STRENGTH}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
+    EXP_NAME="dpo_mask_${MASK_TYPE}_s_${MASK_STRENGTH}_t_${MASK_THRESHOLD_PROB}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
 
     # ------------------ Train ------------------
     python -u train.py \
--- src/score.sh
+++ src/score.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 BASE_DIR="./exp_results"
 INPUT_NAME="prob_test_gen_response.jsonl"
