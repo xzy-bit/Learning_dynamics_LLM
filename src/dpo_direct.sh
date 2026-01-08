c1a6d28c54cb5bd0661c4f52c489af39abdade16
xzy-bit <buxzyhangaa@gamil.com>
2026-01-08 10:57:19 +0800

    0108

--- src/dpo_direct.sh
+++ src/dpo_direct.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=2
 export CUDA_LAUNCH_BLOCKING=1
 #MODEL="pythia410m"
 MODEL="qwen18"
@@ -9,20 +9,14 @@ N_EXAMPLES=30000
 EVAL_EVERY=80000
 #DATE=$(date +%m%d)
 DATE="0107"
-ALPHA=1.5
-ENT_BETA=1.0
-USING_NS=true
-LAMBDAS=(0.0)
+LAMBDAS=(0.01 0.1 0.2)
 for LAMBDA in "${LAMBDAS[@]}"; do
     python -u train.py \
-        loss=ent_dpo \
+        loss=dpo \
         loss.beta=0.1 \
-        loss.alpha=$ALPHA \
-        loss.ent_beta=$ENT_BETA\
-        loss.using_ns=$USING_NS\
     	datasets=$DATASET \
         model=$MODEL \
-    	exp_name="dpo_mixed_entmax_l_${LAMBDA}_a_${ALPHA}_usingNs_${USING_NS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}" \
+    	exp_name="dpo_mixed_l_${LAMBDA}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}" \
         trainer=BasicTrainer \
         n_epochs=$N_EPOCHS \
         n_examples=$N_EXAMPLES \
@@ -30,16 +24,16 @@ for LAMBDA in "${LAMBDAS[@]}"; do
         ce_lambda=$LAMBDA \
         save_ckp=true \
         eval_every=$EVAL_EVERY
-        
 done
 
 for LAMBDA in "${LAMBDAS[@]}"; do
+
     python -u gen_multipt.py \
         model=${MODEL} \
-        model.archive="dpo_mixed_entmax_l_${LAMBDA}_a_${ALPHA}_usingNs_${USING_NS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
-        exp_name="eval_dpo_mixed_entmax_l_${LAMBDA}_a_${ALPHA}_usingNs_${USING_NS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
-done
+        model.archive="dpo_mixed_l_${LAMBDA}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}" \
+        exp_name="eval_dpo_mixed_l_${LAMBDA}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
 
+done
 #bash score.sh
 
 
