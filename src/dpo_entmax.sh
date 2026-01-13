e10b250cd610f618a11e71a92f130fcce1c3c3e7
xzy-bit <buxzyhangaa@gamil.com>
2026-01-13 10:55:00 +0800

    0113

--- src/dpo_entmax.sh
+++ src/dpo_entmax.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1
 export CUDA_LAUNCH_BLOCKING=1
 #MODEL="pythia410m"
 MODEL="llama3_1"
@@ -8,14 +8,16 @@ DATASET="hh"
 N_EPOCHS=6
 N_EXAMPLES=30000
 EVAL_EVERY=40000
-DATE="0109"
-ALPHA=1.5
+DATE="0111"
+#ALPHA=1.5
 
 #USING_NS=false
 USING_NS=true
-BETAS=(0.0 0.1 0.25)
 
-for BETA in "${BETAS[@]}"; do
+ALPHAS=(1.75 2.0)
+BETA=0.5
+
+for ALPHA in "${ALPHAS[@]}"; do
     python -u train.py \
     	loss=ent_dpo \
 	loss.beta=0.1 \
@@ -34,9 +36,10 @@ for BETA in "${BETAS[@]}"; do
 	
 done
 
-for BETA in "${BETAS[@]}"; do
+for ALPHA in "${ALPHAS[@]}"; do
     python -u gen_multipt.py \
         model=${MODEL} \
         model.archive="dpo_entmax_${ALPHA}_beta_${BETA}_usingNs_${USING_NS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
         exp_name="eval_dpo_entmax_${ALPHA}_beta_${BETA}_usingNs_${USING_NS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
 done
+score.sh
