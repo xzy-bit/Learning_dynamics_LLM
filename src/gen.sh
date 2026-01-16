7a61df73f98e6ba983cee1638a53772ee8f37134
xzy-bit <buxzyhangaa@gamil.com>
2026-01-16 19:57:54 +0800

    0116

--- src/gen.sh
+++ src/gen.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=1
 python -u gen_inference.py \
 	model=llama3_1\
-	model.archive=sft_llama3_1_hh_ep2_0109\
-	exp_name=eval_sft_llama3_1_hh_ep2_0109
+	model.archive=dpo_pos_1_llama3_1_hh_ep10_0116\
+	exp_name=eval_dpo_pos_1_llama3_1_hh_ep10_0116
--- src/sft.sh
+++ src/sft.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 MODEL="llama3_1"
 #MODEL="qwen18"
 #MODEL="pythia410m"
@@ -6,44 +6,43 @@ DATASET="hh"
 #DATASET="ultrafb"
 TRAINER="BasicTrainer"
 TRAIN_SPLIT="train_dpo"
-N_EPOCHS=2
-N_EXAMPLES=10000
+SFT_EPOCHS=1
+N_EXAMPLES=5000
 EVAL_EVERY=20000
-DATE="0109"
-:<<sft
-python -u train.py \
-  model=$MODEL \
-  exp_name="sft_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}" \
-  datasets=$DATASET \
-  trainer="BasicTrainer" \
-  train_split="train_dpo" \
-  n_epochs=$N_EPOCHS \
-  n_examples=$N_EXAMPLES \
-  save_ckp="true" \
-  eval_every=$EVAL_EVERY
-sft
+DATE="0116"
 
-export CUDA_LAUNCH_BLOCKING=1
-SFT_EPOCHS=2
-N_EPOCHS=6
-N_EXAMPLES=30000
+#python -u train.py \
+#  model=$MODEL \
+#  exp_name="sft_${MODEL}_${DATASET}_ep${SFT_EPOCHS}_${DATE}" \
+#  datasets=$DATASET \
+#  trainer="BasicTrainer" \
+#  train_split="train_dpo" \
+#  n_epochs=$SFT_EPOCHS \
+#  n_examples=$N_EXAMPLES \
+#  save_ckp="true" \
+#  eval_every=$EVAL_EVERY
+
+
+#export CUDA_LAUNCH_BLOCKING=1
+N_EPOCHS=10
+N_EXAMPLES=50000
 EVAL_EVERY=50000
-python -u train.py \
-    loss=dpo \
-    loss.beta=0.1 \
-    datasets=$DATASET \
-    model=$MODEL \
-    exp_name="dpo_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
-    trainer=BasicTrainer \
-    n_epochs=$N_EPOCHS \
-    n_examples=$N_EXAMPLES \
-    model.archive="sft_${MODEL}_${DATASET}_ep${SFT_EPOCHS}_${DATE}"\
-    save_ckp=true \
-    eval_every=$EVAL_EVERY
+#python -u train.py \
+#    loss=dpo \
+#    loss.beta=0.1 \
+#    datasets=$DATASET \
+#    model=$MODEL \
+#    exp_name="dpo_pos_${SFT_EPOCHS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
+#    trainer=BasicTrainer \
+#    n_epochs=$N_EPOCHS \
+#    n_examples=$N_EXAMPLES \
+#    model.archive="sft_${MODEL}_${DATASET}_ep${SFT_EPOCHS}_${DATE}"\
+#    save_ckp=true \
+#    eval_every=$EVAL_EVERY
 
 python -u gen_multipt.py \
    model=${MODEL} \
-   model.archive="dpo_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
-   exp_name="eval_dpo_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
+   model.archive="dpo_pos_${SFT_EPOCHS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
+   exp_name="eval_dpo_pos_${SFT_EPOCHS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
 
-#bash score.sh
+bash score.sh
