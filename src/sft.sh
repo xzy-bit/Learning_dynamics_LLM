d47456d52966ab9cf2375ff4a88dd226a1860844
xzy-bit <buxzyhangaa@gamil.com>
2026-01-20 15:44:42 +0800

    0120

--- src/sft.sh
+++ src/sft.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 MODEL="llama3_1"
 #MODEL="qwen18"
 #MODEL="pythia410m"
@@ -7,26 +7,24 @@ DATASET="hh"
 TRAINER="BasicTrainer"
 TRAIN_SPLIT="train_dpo"
 SFT_EPOCHS=2
-N_EXAMPLES=2000
+N_EXAMPLES=4000
 EVAL_EVERY=20000
-DATE="0116"
+DATE="0120"
 
-<<sft
 python -u train.py \
   model=$MODEL \
-  exp_name="sft_ches_ratio_0_2_${MODEL}_${DATASET}_ep${SFT_EPOCHS}_${DATE}" \
+  exp_name="sft_ches_ratio_0_4_${MODEL}_${DATASET}_ep${SFT_EPOCHS}_${DATE}" \
   datasets=$DATASET \
   trainer="BasicTrainer" \
-  train_split="train_ches_0_2" \
+  train_split="train_ches_0_4" \
   n_epochs=$SFT_EPOCHS \
   n_examples=$N_EXAMPLES \
   save_ckp="true" \
   eval_every=$EVAL_EVERY
-sft
 
 export CUDA_LAUNCH_BLOCKING=1
 N_EPOCHS=10
-N_EXAMPLES=5000
+N_EXAMPLES=40000
 EVAL_EVERY=50000
 
 python -u train.py \
@@ -34,9 +32,10 @@ python -u train.py \
     loss.beta=0.1 \
     datasets=$DATASET \
     model=$MODEL \
-    exp_name="dpo_ches_ratio_0_1_direct_${SFT_EPOCHS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
+    exp_name="dpo_ches_ratio_0_4_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
     trainer=BasicTrainer \
-    train_split="train_ches" \
+    train_split="train_ches_0_4" \
+    model.archive="sft_ches_ratio_0_4_${MODEL}_${DATASET}_ep${SFT_EPOCHS}_${DATE}" \
     n_epochs=$N_EPOCHS \
     n_examples=$N_EXAMPLES \
     save_ckp=true \
@@ -44,8 +43,8 @@ python -u train.py \
 
 python -u gen_multipt.py \
    model=${MODEL} \
-   model.archive="dpo_ches_ratio_0_1_direct_${SFT_EPOCHS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
-   exp_name="eval_dpo_ches_ratio_0_1_direct_${SFT_EPOCHS}_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
+   model.archive="dpo_ches_ratio_0_4_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"\
+   exp_name="eval_dpo_ches_ratio_0_4_${MODEL}_${DATASET}_ep${N_EPOCHS}_${DATE}"
 
 bash score.sh
 
