8389251f223a4fada0503397ad69c912134a00b6
xzy-bit <buxzyhangaa@gamil.com>
2026-01-29 15:24:27 +0800

    0129

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,4 +1,6 @@
-bash run_filtered_base.sh
-bash run_filtered_extend.sh
+#bash run_filtered_base.sh
+#bash run_filtered_extend.sh
 #bash run_llama32_base.sh
 #bash run_llama32_extend.sh
+bash run_llama32_base_full_aligned.sh
+bash run_llama32_base_full_aligned_sft.sh
